a72215b94497acef6a32f34620790750869113b0
Kudakwashe Shambare <kudahshambare263@gmail.com>
2023-03-09 23:10:53 +0200

    FizzBuzz Completed

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -11,6 +11,33 @@
 int fizzbuzz(int n)
 {
 
+int counter = 0;
+  	for(int i=0;i<=n;i++){
+
+  		
+	
+
+	 if(i%3==0){
+		printf("Fizz\n");
+
+	}
+
+	else if(i%5==0){
+		printf("Buzz\n");
+	}
+	
+else if(i%3==0 && n%5==0){
+		printf("FizzBuzz\n");
+	}
+	else {
+		counter++;
+	}
+
+  	}
+  	return counter;
+
+
+
 }
 
 #ifndef TESTING
